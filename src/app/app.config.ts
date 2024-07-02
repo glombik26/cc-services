ecd7cab5cffba9fe79c82b1dca53eb7f26fbe873
Michael Glombik <michael.glombik@netempire.software>
2024-07-02 16:36:58 +0000

    use env variables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,13 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2qwWQH6-tP-rrw0R3UoJmhQk7dAUABF4",
-  authDomain: "cc-services-296ce.firebaseapp.com",
-  projectId: "cc-services-296ce",
-  storageBucket: "cc-services-296ce.appspot.com",
-  messagingSenderId: "413065106728",
-  appId: "1:413065106728:web:1a933ed8a9352ee33c6c1d",
-  measurementId: "G-FYK0W3VR9C"
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
+  measurementId: process.env['MEASUREMENT_ID'],
 };
 
 export const appConfig: ApplicationConfig = {
--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -9,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2qwWQH6-tP-rrw0R3UoJmhQk7dAUABF4",
-  authDomain: "cc-services-296ce.firebaseapp.com",
-  projectId: "cc-services-296ce",
-  storageBucket: "cc-services-296ce.appspot.com",
-  messagingSenderId: "413065106728",
-  appId: "1:413065106728:web:1a933ed8a9352ee33c6c1d",
-  measurementId: "G-FYK0W3VR9C"
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
+  measurementId: process.env['MEASUREMENT_ID'],
 };
 
 
